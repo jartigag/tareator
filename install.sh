9056db693da1d6ad7c061a4c80a4841af3ea3860
jartigag <jartigag@pm.me>
2020-03-16 10:57:26 +0100

    install.sh improved

--- install.sh
+++ install.sh
@@ -3,11 +3,14 @@
 # if external python libraries are needed in a future,
 # they can be easily virtualenv-ed here ( "$@" )
 
+tasks_file=${1:-'\$1'} #  tasks_file will be hardcoded if $1 is non-empty.
+                       #  otherwise, it can be passed as an argument (tareator $1)
+
 cat << EOF > /tmp/tareator
 #!/bin/bash
 
 cd $(pwd)
-python3 -m tareator \$1
+python3 -m tareator "$tasks_file"
 EOF
 
 echo "Generating /usr/bin/tareator... (may ask for sudo password)"
